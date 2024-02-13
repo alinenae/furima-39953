1d280244f7f1470be5b500dd0f5d61f97b8c6974
alinenae <itonaemialine@yahoo.co.jp>
2024-02-13 22:46:21 +0900

    コメントアウトとアソシエーションの削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,10 +14,6 @@ class User < ApplicationRecord
     validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/, message: "全角カタカナで入力して下さい" }
   end
 
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
   validates :nickname, presence: true
   validates :birthday, presence: true
 
