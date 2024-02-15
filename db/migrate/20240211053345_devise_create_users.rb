3b7afa8e1018aac997338fac666107ec3ee396ac
alinenae <itonaemialine@yahoo.co.jp>
2024-02-15 20:53:24 +0900

    リセット

--- db/migrate/20240211053345_devise_create_users.rb
+++ db/migrate/20240211053345_devise_create_users.rb
@@ -4,7 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :last_name,          null:  false
+      t.string :last_name,          null: false
       t.string :first_name,         null: false
       t.string :last_name_kana,     null: false
       t.string :first_name_kana,    null: false
