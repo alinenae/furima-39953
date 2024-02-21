1f68aef6a02aa804056d522247b57bb772f88895
alinenae <itonaemialine@yahoo.co.jp>
2024-02-21 21:03:50 +0900

    複数回使用する変数をメソッド化

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :move_to_index, only: [:edit, :update]
+  before_action :set_item, only: [:edit, :show, :update]
 
   def index
     @item = Item.all.order(created_at: :desc)
@@ -20,15 +21,14 @@ class ItemsController < ApplicationController
   end
   
   def show
-    @item = Item.find(params[:id])
+    
   end
 
   def edit
-    @item = Item.find(params[:id])
+    
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item)
     else
@@ -39,6 +39,10 @@ class ItemsController < ApplicationController
   private
   def item_params
     params.require(:item).permit(:items_name,:explanation,:category_id,:items_state_id,:shipping_burden_id,:territory_id,:carry_dey_id,:price,:item_image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 
   def move_to_index
