e2748992610c8257a0953e05978b8a5c374b3d1a
alinenae <itonaemialine@yahoo.co.jp>
2024-03-02 23:24:06 +0900

    ログアウト機能

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
+//= require rails-ujs
 import "@hotwired/turbo-rails"
 import "./controllers"
 import "./price"
 import "card"
-import Rails from 'rails-ujs'
-Rails.start()
