d78451f3b3a68a596f9c517d4a600ac67f1cacd6
alinenae <itonaemialine@yahoo.co.jp>
2024-03-02 20:05:37 +0900

    ルーティング修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'articles/index'
-  get 'articles/new'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'items/index'
   root to: "items#index"
 
   resources :items do
